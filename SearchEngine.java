0be6422d3340204183e2bf0c80c674a2a3327222
keliang11 <122491370+keliang11@users.noreply.github.com>
2023-01-25 20:39:44 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.ArrayList;
+
+class Handler implements URLHandler{
+    // first find out what's after /add?s=
+    //put these into an array
+    //then find out what's after /search?s=
+    //use .contains!!
+    //turn that into substring
+    //see which strings in the array has the substring
+    List<String> strings = new ArrayList<String>();
+
+    public String handleRequest(URI url){
+
+    
+        if (url.getPath().contains("/add")) {
+            String[] newString = url.getQuery().split("=");
+            strings.add(newString[0]);
+        }
+        else if (url.getPath().equals("/search")) {
+            String[] substring = url.getQuery().split("=");
+            for(int i = 0; i < strings.size(); i++){
+                if(strings.get(i) == substring[0]){
+                    System.out.println(strings.get(i));
+                    return String.format(strings.get(i));
+                }
+            }
+        }else{
+            return "404 Not Found!";
+        }
+        return null;
+        
+            
+
+        
+    }
+    
+}
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
